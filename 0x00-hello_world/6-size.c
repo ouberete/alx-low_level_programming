302449fef4960fb11dc28d529305702b773ee3c3
ouberete <oumberete09@gmail.com>
2023-02-20 14:25:56 -0800

    --amend

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints the size of various types on
+ * the computer it is compiled and run on.
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
