839b82dde853f3b9d7738502a4a00c8161ad3917
ouberete <oumberete09@gmail.com>
2023-03-01 23:50:46 -0800

    --amend

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
+/**
+ *reset_to_98 - a function that takes a pointer to an int as parameter
+ *and update the value it pointkis to 98         
+ */
+
 void reset_to_98(int *n)
 {
-	/**
-	 * reset_to_98 - a function that takes a pointer to an int as parameter
-	 * and update the value it point to 98 
-	 */
-
 	*n = 98;
 }
